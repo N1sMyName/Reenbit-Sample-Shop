8bbfe7c7ba521b7737a7c85c3f142f684c7d3ab4
N1sMyName <Erst6Ste1n@gmail.com>
2022-03-28 12:00:20 +0300

    small fixes

---
+++ src/app/static-nav-top/nav-select/nav-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+@Component({
+  selector: 'style-nav-select',
+  templateUrl: './nav-select.component.html',
+  styleUrls: ['./nav-select.component.sass']
+})
+export class NavSelectComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
