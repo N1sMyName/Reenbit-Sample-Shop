45b66f9947a468d74727cf034b36d32702a1dca9
N1sMyName <Erst6Ste1n@gmail.com>
2022-04-01 01:13:47 +0300

    Auto-generated commit [ci skip]

--- src/app/cart/cart-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CartComponent } from './cart.component';
-
-@NgModule({
-  imports: [RouterModule.forChild([{ path: '', component: CartComponent }])],
-   exports: [RouterModule],
-})
-export class CartRoutingModule {}
